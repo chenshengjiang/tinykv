bddc50eed290c8c3c8e133bb1860f9f9fc891903
augety <1628757522@qq.com>
2023-05-02 13:36:49 +0800

    pro1

--- kv/server/raw_api.go
+++ kv/server/raw_api.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"context"
+
+	"github.com/pingcap-incubator/tinykv/kv/storage"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
 )
 
@@ -11,26 +13,94 @@ import (
 // RawGet return the corresponding Get response based on RawGetRequest's CF and Key fields
 func (server *Server) RawGet(_ context.Context, req *kvrpcpb.RawGetRequest) (*kvrpcpb.RawGetResponse, error) {
 	// Your Code Here (1).
-	return nil, nil
+	reader, err := server.storage.Reader(req.Context)
+	if reader != nil {
+		defer reader.Close()
+	}
+	if err != nil {
+		return nil, err
+	}
+	val, err := reader.GetCF(req.Cf, req.Key)
+	if err != nil {
+		return nil, err
+	}
+	resp := &kvrpcpb.RawGetResponse{
+		Value:    val,
+		NotFound: false,
+	}
+	if val == nil {
+		resp.NotFound = true
+	}
+	return resp, nil
 }
 
 // RawPut puts the target data into storage and returns the corresponding response
 func (server *Server) RawPut(_ context.Context, req *kvrpcpb.RawPutRequest) (*kvrpcpb.RawPutResponse, error) {
 	// Your Code Here (1).
 	// Hint: Consider using Storage.Modify to store data to be modified
-	return nil, nil
+	put := storage.Put{
+		Key:   req.Key,
+		Value: req.Value,
+		Cf:    req.Cf,
+	}
+	// 用put初始化一个Modify
+	batch := storage.Modify{Data: put}
+	err := server.storage.Write(req.Context, []storage.Modify{batch})
+	if err != nil {
+		return nil, err
+	}
+	return &kvrpcpb.RawPutResponse{}, nil
 }
 
 // RawDelete delete the target data from storage and returns the corresponding response
 func (server *Server) RawDelete(_ context.Context, req *kvrpcpb.RawDeleteRequest) (*kvrpcpb.RawDeleteResponse, error) {
 	// Your Code Here (1).
 	// Hint: Consider using Storage.Modify to store data to be deleted
-	return nil, nil
+	// 和RawPut同理
+	delete := storage.Delete{
+		Key: req.Key,
+		Cf:  req.Cf,
+	}
+	batch := storage.Modify{Data: delete}
+	err := server.storage.Write(req.Context, []storage.Modify{batch})
+	if err != nil {
+		return nil, err
+	}
+	return &kvrpcpb.RawDeleteResponse{}, nil
 }
 
 // RawScan scan the data starting from the start key up to limit. and return the corresponding result
 func (server *Server) RawScan(_ context.Context, req *kvrpcpb.RawScanRequest) (*kvrpcpb.RawScanResponse, error) {
 	// Your Code Here (1).
 	// Hint: Consider using reader.IterCF
-	return nil, nil
+	reader, err := server.storage.Reader(req.Context)
+	if reader != nil {
+		defer reader.Close()
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	iter := reader.IterCF(req.Cf)
+	defer iter.Close()
+
+	cnt := 0
+	var kvs []*kvrpcpb.KvPair
+	// 从startkey开始，返回最多limit个kv，Valid()会判断是否越界了
+	for iter.Seek(req.StartKey); iter.Valid(); iter.Next() {
+		if cnt >= int(req.GetLimit()) {
+			break
+		}
+		item := iter.Item()
+		key := item.Key()
+		val, _ := item.Value()
+		kvs = append(kvs, &kvrpcpb.KvPair{
+			Key:   key,
+			Value: val,
+		})
+		cnt++
+	}
+	return &kvrpcpb.RawScanResponse{
+		Kvs: kvs,
+	}, nil
 }
--- kv/storage/standalone_storage/standalone_storage.go
+++ kv/storage/standalone_storage/standalone_storage.go
@@ -1,8 +1,12 @@
 package standalone_storage
 
 import (
+	"path"
+
+	"github.com/Connor1996/badger"
 	"github.com/pingcap-incubator/tinykv/kv/config"
 	"github.com/pingcap-incubator/tinykv/kv/storage"
+	"github.com/pingcap-incubator/tinykv/kv/util/engine_util"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
 )
 
@@ -10,11 +14,57 @@ import (
 // communicate with other nodes and all data is stored locally.
 type StandAloneStorage struct {
 	// Your Data Here (1).
+	// 对Engine的进一步封装
+	engine *engine_util.Engines
+	conf   *config.Config
+}
+
+// 用来实现StorageReader的接口
+type StandAloneStorageReader struct {
+	Kvtxn *badger.Txn
+}
+
+func NewStandAloneStorageReader(txn *badger.Txn) *StandAloneStorageReader {
+	return &StandAloneStorageReader{
+		Kvtxn: txn,
+	}
+}
+
+func (s *StandAloneStorageReader) GetCF(cf string, key []byte) ([]byte, error) {
+	val, err := engine_util.GetCFFromTxn(s.Kvtxn, cf, key)
+	// 这里需要屏蔽掉 key not found 错误
+	// 因为上层认为这并不是error
+	if err == badger.ErrKeyNotFound {
+		return nil, nil
+	}
+	return val, err
+}
+
+func (s *StandAloneStorageReader) IterCF(cf string) engine_util.DBIterator {
+	// func NewCFIterator(cf string, txn *badger.Txn) *BadgerIterator
+	return engine_util.NewCFIterator(cf, s.Kvtxn)
+	// 返回的BadgerIterator类型其实就相当于DBIterator,因为其实现了DBIterator所有的接口函数
+}
+
+func (s *StandAloneStorageReader) Close() {
+	s.Kvtxn.Discard()
 }
 
 func NewStandAloneStorage(conf *config.Config) *StandAloneStorage {
 	// Your Code Here (1).
-	return nil
+	dbpath := conf.DBPath
+
+	// 每个节点都会有两个badgerdb，Engine里有说明，所以这里要创建两个DB
+	kvpath := path.Join(dbpath, "kv")
+	kv := engine_util.CreateDB(kvpath, false)
+
+	raftpath := path.Join(dbpath, "raft")
+	raft := engine_util.CreateDB(raftpath, true)
+
+	return &StandAloneStorage{
+		engine: engine_util.NewEngines(kv, raft, kvpath, raftpath),
+		conf:   conf,
+	}
 }
 
 func (s *StandAloneStorage) Start() error {
@@ -24,15 +74,34 @@ func (s *StandAloneStorage) Start() error {
 
 func (s *StandAloneStorage) Stop() error {
 	// Your Code Here (1).
+	if err := s.engine.Close(); err != nil {
+		return err
+	}
 	return nil
 }
 
-func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader, error) {
+func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader, error) { // 这里要返回一个StorageReader，也就需要实现它相应的接口函数
 	// Your Code Here (1).
-	return nil, nil
+	// For read-only transactions, set update to false.
+	txn := s.engine.Kv.NewTransaction(false)
+	return NewStandAloneStorageReader(txn), nil
 }
 
 func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) error {
 	// Your Code Here (1).
+	// 需要将每个 Modify 根据类型进行 put or delete
+	var err error
+	for _, m := range batch {
+		cf, key, val := m.Cf(), m.Key(), m.Value()
+		switch m.Data.(type) {
+		case storage.Put:
+			err = engine_util.PutCF(s.engine.Kv, cf, key, val)
+		case storage.Delete:
+			err = engine_util.DeleteCF(s.engine.Kv, cf, key)
+		}
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
